04c7342a697664e66a5aa54f841b7072920eb080
Gnlow <entrydark@gmail.com>
2020-05-17 15:21:03 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+class Discrete {
+    constructor(...items){
+        this.values = []
+        this.weights = []
+        let acc = 0
+        items.forEach(
+            ([value, weight]) => {
+                this.values.push(value)
+                this.weights.push(acc += weight)
+            }
+        )
+    }
+    rand(seed){
+        const key = seed * this.weights[this.weights.length-1]
+        let i = 0
+        while(this.weights[i]<key){
+            i++
+        }
+        return this.values[i]
+    }
+}
